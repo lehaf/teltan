fc2c26967336e86c9a58215f74c1be55ac996b3a
alexf <a.f@webcompany.by>
2023-07-21 13:45:41 +0300

    fix translate on personal page

--- personal/rate/lang/he/.section.php
+++ personal/rate/lang/he/.section.php
@@ -2,7 +2,7 @@
 $MESS ['CABINET'] = "מודעות שלי";
 $MESS ['MESSAGES'] = "הודעות שלי";
 $MESS ['BUY_HISTORY'] = "ארנק";
-$MESS ['BUY_TARIF'] = "היסטוריה";
+$MESS ['BUY_TARIF'] = "חבילות";
 $MESS ['TRACKED'] = "במעקב";
 $MESS ['DATA_CHANGE'] = "שינוי פרטי התקשרות";
 $MESS ['CHANGE_PASS'] = "שנה סיסמא";
