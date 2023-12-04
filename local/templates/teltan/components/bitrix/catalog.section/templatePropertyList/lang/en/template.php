7271b0ab7b1b75a455920eab688840e2e0e63790
lehaf <lehaforever9@bk.ru>
2023-12-05 01:46:26 +0300

    delete extra catalog.section templates

--- local/templates/teltan/components/bitrix/catalog.section/templatePropertyList/lang/en/template.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "All the information linked to this record will be deleted. Continue anyway?";
-$MESS["TODAY"] = "Today";
-$MESS["YESTERDAY"] = "Yesterday";
-$MESS["map"] = "Show ads on the map";
-?>
