dc0ffbf39fa29fe5006f70f6b96b59b0f81835e3
lehaf <lehaforever9@bk.ru>
2023-12-04 14:41:55 +0300

    fix ribbon type hlblock error 2 part

--- local/templates/teltan/components/bitrix/news.list/template_ad_in_section/result_modifier.php
+++ local/templates/teltan/components/bitrix/news.list/template_ad_in_section/result_modifier.php
@@ -9,7 +9,7 @@ foreach ($arResult['ITEMS'] as $k => $arItem){
 
 
     if($arItem['PROPERTIES']['TYPE_TAPE']['VALUE'])
-        $arResult['ITEMS'][$k]['TAPE'] = getHighloadInfo(TYPE_TAPES_ID, array('select' => array('*'), 'filter' => array('UF_ACTIVE' => 1, 'UF_XML_ID' => $arItem['PROPERTIES']['TYPE_TAPE']['VALUE'])))[0];
+        $arResult['ITEMS'][$k]['TAPE'] = getHighloadInfo(PERSONAL_RIBBON_HL_ID, array('select' => array('*'), 'filter' => array('UF_ACTIVE' => 1, 'UF_XML_ID' => $arItem['PROPERTIES']['TYPE_TAPE']['VALUE'])))[0];
 
     $arResult['ITEMS'][$k]['SECTION'] = getSectionInfo($arItem['IBLOCK_SECTION_ID'], $arItem['IBLOCK_ID']);
 }
