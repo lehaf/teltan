43d63015524b1abd6245dcaa81fc370e6c507b40
lehaf <lehaforever9@bk.ru>
2024-01-05 16:31:31 +0300

    edited 404.php file

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
-<?
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404","Y");
-
+$APPLICATION->SetTitle('לא נמצא');
 ?>
-<h1 style="padding-left: 450px;padding-bottom: 200px;padding-top: 100px;">404 not found</h1>
+<h1 style="padding-left: 450px;padding-bottom: 200px;padding-top: 100px;">404 לא נמצא</h1>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 
