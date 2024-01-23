06deac3076f9fb89f4da3071072e7c13f1f064b2
lehaf <lehaforever9@bk.ru>
2024-01-23 15:02:56 +0300

    hided language switcher from header

--- local/templates/teltan/header.php
+++ local/templates/teltan/header.php
@@ -31,7 +31,6 @@ else
     <?php $APPLICATION->ShowHead()?>
     <title><?php $APPLICATION->ShowTitle()?></title>
 </head>
-
 <body>
 <div id="panel"><?php $APPLICATION->ShowPanel();?></div>
 
@@ -76,37 +75,27 @@ else
             <div class="row">
                 <div class="col col-lg-5 col-xl-6">
                     <div class="d-flex h-100 justify-content-between align-items-center">
-                        <?php $APPLICATION->IncludeComponent(
-                            "bitrix:main.include",
-                            "",
-                            Array(
-                                "AREA_FILE_SHOW" => "file",
-                                "AREA_FILE_SUFFIX" => "inc",
-                                "EDIT_TEMPLATE" => "",
-                                "PATH" => SITE_TEMPLATE_PATH."/includes/".SITE_ID."/header/languages_list.php"
-                            )
-                        );?>
-
-                        <div class="d-none d-lg-flex align-items-center" style="visibility: hidden">
-
-                            <a class="d-flex align-items-center mr-2 pr-2 mr-lg-3 pr-lg-3 mr-xl-5 pr-xl-5 border-right" href="#">
-                                <span class="mr-2"><?=Loc::getMessage('CATALOG');?></span> <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M22.3556 12.0919H18.3488V7.20703C18.3488 6.30234 17.6128 5.56641 16.7081 5.56641H13.2754V2.10914C13.2754 0.946172 12.3292 0 11.1662 0H7.18256C6.01959 0 5.07342 0.946172 5.07342 2.10914V5.56641H1.64063C0.735938 5.56641 0 6.30234 0 7.20703V22.3594C0 23.2641 0.735938 24 1.64063 24C13.1446 24 21.8296 23.9963 22.3556 23.9963C23.2603 23.9963 23.9963 23.2603 23.9963 22.3556V13.732C23.9963 12.8273 23.2603 12.0919 22.3556 12.0919ZM7.18256 1.40625H11.1662C11.5538 1.40625 11.8691 1.72158 11.8691 2.10914V5.56641H6.47967V2.10914C6.47967 1.72158 6.795 1.40625 7.18256 1.40625ZM1.64063 22.5938C1.51125 22.5938 1.40625 22.4888 1.40625 22.3594V7.20703C1.40625 7.07766 1.51125 6.97266 1.64063 6.97266H5.07342V9.04542C5.07342 9.43373 5.38824 9.74855 5.77655 9.74855C6.16486 9.74855 6.47967 9.43373 6.47967 9.04542V6.97266H11.8691V9.04542C11.8691 9.43373 12.1839 9.74855 12.5723 9.74855C12.9606 9.74855 13.2754 9.43373 13.2754 9.04542V6.97266H16.7081C16.8375 6.97266 16.9425 7.07766 16.9425 7.20703V12.0919H10.1156C9.21094 12.0919 8.475 12.8273 8.475 13.732V22.3556C8.475 22.4367 8.4811 22.5159 8.49234 22.5938H1.64063ZM22.59 22.3556C22.59 22.4845 22.485 22.59 22.3556 22.59C21.9663 22.59 10.6801 22.59 10.1156 22.59C9.98672 22.59 9.88125 22.4845 9.88125 22.3556V13.732C9.88125 13.6031 9.98672 13.4981 10.1156 13.4981H22.3556C22.485 13.4981 22.59 13.6031 22.59 13.732V22.3556Z" fill="#2A2A2A"/>
-                                    <path d="M19.376 15.1378C18.9879 15.1378 18.6729 15.4528 18.6729 15.8409V17.9269C18.6729 18.1748 18.5435 18.3928 18.349 18.518C18.1121 18.6715 18.2753 18.6295 14.502 18.6295C14.1143 18.6295 13.7988 18.3141 13.7988 17.9269V15.8409C13.7988 15.4528 13.4843 15.1378 13.0957 15.1378C12.7076 15.1378 12.3926 15.4528 12.3926 15.8409V17.9269C12.3926 19.0898 13.339 20.0358 14.502 20.0358C18.3048 20.0358 18.0909 20.0487 18.349 20.0016C19.332 19.8225 20.0791 18.9605 20.0791 17.9269V15.8409C20.0791 15.4528 19.7641 15.1378 19.376 15.1378Z" fill="#2A2A2A"/>
-                                </svg>
-
-                            </a>
-
-                            <a class="d-flex align-items-center mr-2 pr-2 pr-lg-3 pr-xl-5 border-right" href="#">
-                                <span class="mr-2"><?=Loc::getMessage('DRAWS');?></span> <i class="mb-1 icon-giftbox-1"></i>
-                            </a>
-                        </div>
+                        <?php
+                        // Переключатель языков
+//                        $APPLICATION->IncludeComponent(
+//                            "bitrix:main.include",
+//                            "",
+//                            Array(
+//                                "AREA_FILE_SHOW" => "file",
+//                                "AREA_FILE_SUFFIX" => "inc",
+//                                "EDIT_TEMPLATE" => "",
+//                                "PATH" => SITE_TEMPLATE_PATH."/includes/header/languages_list.php"
+//                            )
+//                        );
+                        ?>
                     </div>
                 </div>
-
                 <div class="col-7 col-xl-6">
                     <div class="d-flex justify-content-end justify-content-lg-between align-items-center">
-                        <a href="#" class="mr-auto mr-md-5 mr-lg-0 text-uppercase btn-your-add" data-toggle="modal" data-target="#<?=($IDUser) ? 'modalTypeAdd' : 'logInModal';?>">
+                        <a href="#" class="mr-auto mr-md-5 mr-lg-0 text-uppercase btn-your-add"
+                           data-toggle="modal"
+                           data-target="#<?=($IDUser) ? 'modalTypeAdd' : 'logInModal';?>"
+                        >
                             <i class="icon-user-1 mr-2"></i>
                             <span><?=Loc::getMessage('YOU_AD');?></span>
                         </a>
@@ -215,17 +204,14 @@ else
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-
                 <p class="mb-3 modal-title subtitle" id="staticBackdropLabel">סוג מודעה</p>
             </div>
-
             <div class="p-4 p-lg-3 modal-body">
                 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
                     <div class="col">
                         <p class="h6 font-weight-bold">
                             <?=Loc::getMessage('other')?>
                         </p>
-
                         <div class="d-flex flex-column">
                             <a href="<?=$GLOBALS['arSetting'][SITE_ID]['href'];?>add/fm/" class="btn btn-primary text-uppercase font-weight-bold mb-4">
                                 <?=Loc::getMessage('yourAds')?>
@@ -273,15 +259,15 @@ else
 <?php if(!CSite::InDir('/property/')) {?>
     <main class="mb-5">
         <?php if (!CSite::InDir('/index.php')) {
-         $APPLICATION->IncludeComponent(
-             "bitrix:breadcrumb",
-             "template_breadcrumbs",
-             array(
-                 "PATH" => "",
-                 "SITE_ID" => SITE_ID,
-                 "START_FROM" => "0"
-             )
-         );
-     }
- }
+             $APPLICATION->IncludeComponent(
+                 "bitrix:breadcrumb",
+                 "template_breadcrumbs",
+                 array(
+                     "PATH" => "",
+                     "SITE_ID" => SITE_ID,
+                     "START_FROM" => "0"
+                 )
+             );
+        }
+}
 ?>
---
+++ local/templates/teltan/includes/header/languages_list.php
@@ -0,0 +1,23 @@
+<div class="d-flex align-items-center">
+    <a class="d-flex align-items-center lang-list" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="mr-2 icon-arrow-down-sign-to-navigate-3"></i>
+        <span class="mr-2 ml-1"><?=LANGUAGE_ID?></span>
+        <i class="icon-language"></i>
+    </a>
+    <div class="dropdown-menu languages">
+        <div class="d-flex justify-content-between align-items-center lang-block">
+            <div class="lang-icon"><img src="<?=SITE_TEMPLATE_PATH?>/assets/lang-ru.png" alt="lang"></div>
+            <a class="border-bottom dropdown-item"href="/?lang=ru">Rus</a>
+        </div>
+
+        <div class="d-flex justify-content-between align-items-center lang-block">
+            <div class="lang-icon"><img src="<?=SITE_TEMPLATE_PATH?>/assets/lang-en.png" alt="lang"></div>
+            <a class="border-bottom dropdown-item" href="/?lang=en">Eng</a>
+        </div>
+
+        <div class="d-flex justify-content-between align-items-center lang-block">
+            <div class="lang-icon"><img src="<?=SITE_TEMPLATE_PATH?>/assets/lang-he.jpg" alt="lang"></div>
+            <a class="border-bottom dropdown-item"href="/?lang=he">Heb</a>
+        </div>
+    </div>
+</div>
